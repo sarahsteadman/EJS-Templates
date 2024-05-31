15cca186da0f14b3db64a1ef8650ff3ffcf39eae
sarahsteadman <fairyfaye4real@gmail.com>
2024-05-31 15:13:20 -0600

    added error handling

--- routes/static.js
+++ routes/static.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const baseController = require('../controllers/baseController');
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
+router.get("/", baseController.buildHome);
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
