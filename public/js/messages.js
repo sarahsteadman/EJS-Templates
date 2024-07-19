e9f4910e2b8268900a8958cc6dca3d231b424f9f
sarahsteadman <fairyfaye4real@gmail.com>
2024-07-18 23:37:02 -0600

    table rows are clickable

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,1 +1,12 @@
-document.getElementById("")
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Dom Loaded");
+    const rows = document.querySelectorAll('.clickable-row');
+
+    rows.forEach(row => {
+        console.log("loop occured");
+        row.addEventListener('click', () => {
+            console.log("Row clicked");
+            window.location.href = row.getAttribute('data-href');
+        });
+    });
+});
